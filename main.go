935ca2bfb0fa26f03be0d3d05920359369bb4632
salamandra19 <tatyana.krivonos@gmail.com>
2017-12-21 17:04:07 +0200

    correct extention

--- main.go
+++ main.go
@@ -1,72 +1,28 @@
 package main
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func main() {
-	log.SetFlags(0)
-	if len(os.Args) != 2 || !isKindle(os.Args[1]) {
-		log.Fatalf("Usage: %s path/to/kindle\n", os.Args[0])
+	files := os.Args[1:]
+	if len(files) != 1 {
+		log.Printf("введите %v\t и корректный адрес каталога", os.Args[0])
+		return
 	}
-	kindleDir := os.Args[1]
-	err := filepath.Walk(kindleDir, filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	matchDirs := []string{".mobi", ".azw", ".pdf", ".prc", ".txt"}
-	err = filepath.Walk(kindleDir, matchFiles(matchDirs))
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	checkAdress(files[0])
+	checkAdress(files[0] + "/documents")
+	checkAdress(files[0] + "/system")
 }
 
-func filePath(path string, info os.FileInfo, err error) error {
+func checkAdress(s string) bool {
+	fileInfo, err := os.Stat(s)
 	if err != nil {
-		log.Print(err)
-		return nil
-	}
-	return nil
-}
+		log.Printf("введите %v\t и корректный адрес каталога", os.Args[0])
+		os.Exit(1)
 
-func matchFiles(matchDirs []string) filepath.WalkFunc {
-	return func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Print(err)
-			return nil
-		}
-		if info.IsDir() {
-			dir := filepath.Base(path)
-			for _, d := range matchDirs {
-				if d != dir {
-					return filepath.SkipDir
-				}
-			}
-		}
-		fmt.Println(path)
-		return nil
 	}
-}
-
-var ErrNotADir = errors.New("not a directory")
-
-func dirExists(dir string) error {
-	fileInfo, err := os.Stat(dir)
-	if err != nil {
-		return err
-	}
-	if !fileInfo.IsDir() {
-		return ErrNotADir
-	}
-	return nil
-}
-
-func isKindle(dir string) bool {
-	return dirExists(dir) == nil &&
-		dirExists(dir+"/documents") == nil &&
-		dirExists(dir+"/system") == nil
+	return fileInfo.IsDir()
 }
