05f468db641d74265de48178e1cbc779439ed1e8
salamandra19 <tatyana.krivonos@gmail.com>
2017-12-21 17:17:08 +0200

    extention

--- main.go
+++ main.go
@@ -1,28 +1,71 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"os"
+	"path/filepath"
+	"regexp"
 )
 
-func main() {
-	files := os.Args[1:]
-	if len(files) != 1 {
-		log.Printf("введите %v\t и корректный адрес каталога", os.Args[0])
-		return
-	}
+var kindleDir string
 
-	checkAdress(files[0])
-	checkAdress(files[0] + "/documents")
-	checkAdress(files[0] + "/system")
+func main() {
+	log.SetFlags(0)
+	if len(os.Args) != 2 || !isKindle(os.Args[1]) {
+		log.Fatalf("Usage: %s path/to/kindle", os.Args[0])
+	}
+	kindleDir = os.Args[1]
+	err := filepath.Walk(kindleDir, filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func checkAdress(s string) bool {
-	fileInfo, err := os.Stat(s)
+func filePath(path string, info os.FileInfo, err error) error {
 	if err != nil {
-		log.Printf("введите %v\t и корректный адрес каталога", os.Args[0])
-		os.Exit(1)
-
+		log.Print(err)
+		return nil
 	}
-	return fileInfo.IsDir()
+	if filepath.Dir(kindleDir) == kindleDir {
+		return filepath.SkipDir
+	}
+	if !(filepath.Ext(path) == (".mobi")) ||
+		!(filepath.Ext(path) == (".azw")) ||
+		!(filepath.Ext(path) == (".prc")) ||
+		!(filepath.Ext(path) == (".pdf")) ||
+		!(filepath.Ext(path) == (".txt")) {
+		return filepath.SkipDir
+	}
+	fmt.Println(path)
+	return nil
+}
+
+func match(s string) bool {
+	re := regexp.MustCompile(".mobi")
+	if !re.MatchString(s) {
+		return false
+	}
+	fmt.Println(re)
+	return true
+}
+
+var ErrNotADir = errors.New("not a directory")
+
+func dirExists(dir string) error {
+	fileInfo, err := os.Stat(dir)
+	if err != nil {
+		return err
+	}
+	if !fileInfo.IsDir() {
+		return ErrNotADir
+	}
+	return nil
+}
+
+func isKindle(dir string) bool {
+	return dirExists(dir) == nil &&
+		dirExists(dir+"/documents") == nil &&
+		dirExists(dir+"/system") == nil
 }
