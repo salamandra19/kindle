295402bca13b4df3af64eeee706decfb723335e5
salamandra19 <tatyana.krivonos@gmail.com>
2018-01-03 12:31:03 +0200

    2018/01/03 12:20:14 goconvey.go:118: Received request from watcher to execute tests... TestAbs2KindlePath

--- main_test.go
+++ main_test.go
@@ -9,6 +9,49 @@ import (
 
 func TestMain(m *testing.M) { check.TestMain(m) }
 
+// - путь path/documents/cat
+// - путь path/documents
+
+func TestAbs2KindlePath(tt *testing.T) {
+	t := check.T(tt)
+	cases := []struct {
+		path string
+		want string
+	}{
+		{"/home/tanya/documents/Author/book.txt", "Author/book.txt"},
+		{"/home/tanya/documents", ""},
+	}
+	for _, v := range cases {
+		got := Abs2KindlePath(v.path)
+		t.Equal(got, v.want)
+	}
+}
+
+/*
+func TestIsKindle(tt *testing.T) {
+	t := check.T(tt)
+// - !IsDir()
+// - путь IsDir, содержит documents, system
+// - путь IsDir, содержит documents, не содержит system
+// - путь IsDir, содержит system, не содержит documents
+// - путь IsDir, не содержит documents, не содержит system
+// - путь !IsDir, содержит documents, system
+// - путь !IsDir, содержит documents, не содержит system
+// - путь !IsDir, содержит system, не содержит documents
+	cases := []struct{
+		dir string
+		want bool
+	}{
+		{"/gocode/src/library",true},
+		{"",true},
+		{"",true},
+		{"",true},
+		{"",true},
+	}
+
+	}
+}
+*/
 // - пустая строка   = !nil
 // - не файл/каталог = !nil
 // - файл            = ErrNotADir
